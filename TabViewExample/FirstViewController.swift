b61e84e35b4c47b0d08f08e2e7284da51f6b8304
moxDroid Labs Inc <pritam.world@gmail.com>
2020-03-23 18:13:04 -0400

    Initial Commit

---
+++ TabViewExample/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  TabViewExample
+//
+//  Created by moxDroid on 2020-03-23.
+//  Copyright © 2020 moxDroid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var imgCharacter: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabViewExample/ThirdViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ThirdViewController.swift
+//  TabViewExample
+//
+//  Created by moxDroid on 2020-03-23.
+//  Copyright © 2020 moxDroid. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var imgCharacter: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = String(100)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
